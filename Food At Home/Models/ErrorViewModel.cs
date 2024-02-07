c4411db36e036c36b1d1de47954847eb7a62e23b
radostin60 <radostindimitrov05@pmgkk.com>
2024-02-07 10:31:20 +0200

    repo init

---
+++ Food At Home/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Food_At_Home.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
