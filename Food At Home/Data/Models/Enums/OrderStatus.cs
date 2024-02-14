0536ded752ea7a40cb4438cafa41a18bdea20360
radostin60 <radostindimitrov05@pmgkk.com>
2024-02-14 11:33:17 +0200

    order entity

---
+++ Food At Home/Data/Models/Enums/OrderStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace Food_At_Home.Data.Models.Enums
+{
+    public enum OrderStatus
+    {
+        Waiting = 0,
+        Confirmed = 1,
+        Sent = 2,
+        Delivered = 3,
+    }
+}
---
+++ Food At Home/Data/Models/Order.cs
@@ -0,0 +1,29 @@
+﻿using Food_At_Home.Data.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Food_At_Home.Data.Models
+{
+    public class Order
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [EnumDataType(typeof(OrderStatus))]
+        public string Status { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Restaurant))]
+        public Guid RestaurantId { get; set; }
+        public Restaurant Restaurant { get; set; }
+
+        [Required]
+        public DateTime OrderTime { get; set; }
+
+        [Required]
+        public DateTime DeliveryTime { get; set; }
+
+
+    }
+}
