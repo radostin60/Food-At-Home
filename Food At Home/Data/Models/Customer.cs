17454ecb2ea30a83dd395ffbc7c7dbc26ffc70a2
radostin60 <radostindimitrov05@pmgkk.com>
2024-02-20 09:38:42 +0200

    20.02.2024

--- Food At Home/Data/Models/Customer.cs
+++ Food At Home/Data/Models/Customer.cs
@@ -1,7 +1,26 @@
-﻿namespace Food_At_Home.Data.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Food_At_Home.Data.Models
 {
     public class Customer
     {
+        public Customer()
+        {
 
+            this.Orders = new List<Order>();
+            this.Payments = new List<Payment>();
+        }
+
+        [Required]
+        public Guid Id { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(User))]
+        public Guid UserId { get; set; }
+        public User User { get; set; } = null!;
+
+        public ICollection<Order> Orders { get; set; }
+        public ICollection<Payment> Payments { get; set; }
     }
 }
