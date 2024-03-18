a6d7d4903470c069b9a0334268e9f17eeecbedbc
radostin60 <150326018+radostin60@users.noreply.github.com>
2024-03-18 11:40:38 +0200

    18.03.2024

--- Food At Home/Services/DishService.cs
+++ Food At Home/Services/DishService.cs
@@ -1,9 +1,36 @@
 ﻿using Food_At_Home.Contracts;
+using Food_At_Home.Data;
+using Food_At_Home.Data.Models;
 
 namespace Food_At_Home.Services
 {
     public class DishService: IDishService
     {
+        private readonly FoodDbContext _context;
 
+        public DishService(FoodDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Dish> CreateDishAsync(CreateDishViewModel model)
+        {
+            var dish = new Dish
+            {
+                Name = model.Name,
+                CategoryId = model.CategoryId,
+                Ingredients = model.Ingredients,
+                Description = model.Description,
+                Price = model.Price,
+                ImageUrl = model.ImageUrl,
+                RestaurantId = model.RestaurantId,
+                Quantity = model.Quantity
+            };
+
+            _context.Dishes.Add(dish);
+            await _context.SaveChangesAsync();
+
+            return dish;
+        }
     }
 }
